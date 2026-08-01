8fa04758f203882d697234189271b0c0c5bc6aae
Steve Musili <musilisteve223@gmail.com>
2026-08-01 15:39:03 +0300

    feat: make initial changes on script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
