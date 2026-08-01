7a61f51fb7a17179aec13693ef1bfb56962adab5
Steve Musili <musilisteve223@gmail.com>
2026-08-01 16:17:50 +0300

    feat: add new changes in script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
+document.getElementById("nameform").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const day = parseInt(document.getElementById("day").value);
+    const month = parseInt(document.getElementById("month").value);
+    const year = parseInt(document.getElementById("year").value);
+
+    const gender = document.querySelector('input[name="gender"]:checked');
+    
+
+
+});
